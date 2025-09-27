640228b504bf3c1a41bbff8aa58f7b03f4a84522
G3ntl3 <hammedlawal412@gmail.com>
2025-09-27 09:51:38 +0100

    avatar not required

--- models/user_model.js
+++ models/user_model.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   wallet: { type: String, unique: true, required: true },
   username: { type: String, required: true , unique:true },
- avatar: { type: String, required: true },
- 
+ avatar: { type: String },
   wins: { type: Number, default: 0 },
   losses: { type: Number, default: 0 },
   earnings: { type: Number, default: 0 },
   history: [
+    
     {
       "betId": { type: Number, default: 0 },
       "marketId":{ type: String, required: true }  ,
